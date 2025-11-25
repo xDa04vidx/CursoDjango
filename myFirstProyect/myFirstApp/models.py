d31343b1a12ca8e80112e642eb38e18731f31ae2
David Arias <davariasc@udistrital.edu.co>
2025-11-25 16:13:38 -0500

    aprendizaje sobre orm y urls

--- myFirstProyect/myFirstApp/models.py
+++ myFirstProyect/myFirstApp/models.py
@@ -16,11 +16,26 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birthDate = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    author = models.OneToOneField(Author,on_delete=models.CASCADE)
+    website = models.URLField()
+    biography = models.TextField(max_length=500)
+
+
 class Book(models.Model):
     title = models.TextField(max_length=200)
     publicationDate = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors =models.ManyToManyField(Author, related_name="authors")
 
     def __str__(self):
         return self.title
+    
--- myFirstProyect/myFirstApp/views.py
+++ myFirstProyect/myFirstApp/views.py
@@ -1,12 +1,26 @@
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 from django.shortcuts import render
+from myFirstApp.models import Car
 
 # Create your views here.
 def myView(request):
-    carList =[
-        {"title":"BMW"},
-        {"title":"Mazda"}
-    ]
+    carList = Car.objects.all().order_by("title")
     context={
         "carList":carList
     }
     return render(request, "myFirstApp/carList.html",context)
+
+class CarListView(TemplateView):
+    template_name = "myFirstApp/carList.html"
+
+    def getContextData(self):
+        carList = Car.objects.all().order_by("title")
+        return {
+            "carList":carList
+        }
+
+def myTestView(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
